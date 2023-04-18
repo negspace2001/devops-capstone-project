cc7b161e79ae888f5afc2d32b1a1d2a0503a6401
negspace2001 <nguimbouseffa@yahoo.fr>
2023-04-18 07:39:05 -0400

    Added CORS headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -236,3 +236,9 @@ class TestAccountService(TestCase):
         self.assertEqual(response.headers.get("X-Content-Type-Options"), "nosniff")
         self.assertEqual(response.headers.get("Content-Security-Policy"), "default-src \'self\'; object-src \'none\'")
         self.assertEqual(response.headers.get("Referrer-Policy"), "strict-origin-when-cross-origin")
+    
+    def test_web_security_origin(self):
+        """ Tests Flask Cors calling the root URL passing in environ_overrides=HTTPS_ENVIRON as param """
+        response = self.client.get("/", environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.headers.get("Access-Control-Allow-Origin"), "*")
